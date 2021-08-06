81a86332fee34ccfd964658d4295296727e74530
包谷清清 <776570787@qq.com>
2021-08-06 22:56:32 +0800

    init

---
+++ src/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <h2>学生姓名：{{ studentName }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      studentName:'大清',
+      age:19
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
